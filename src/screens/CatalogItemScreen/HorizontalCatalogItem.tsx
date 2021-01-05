7b538a0a764b21cf292f971a9063f818b4c22d6f
Alexey <alex303606@gmail.com>
2021-01-05 02:19:55 +0200

    added CntNew

--- src/screens/CatalogItemScreen/HorizontalCatalogItem.tsx
+++ src/screens/CatalogItemScreen/HorizontalCatalogItem.tsx
@@ -5,7 +5,6 @@ import {ICatalogItem} from 'src/store/reducers/catalog';
 import styled from 'styled-components';
 import {Image, Pressable} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
-import {EScreens} from '@interfaces';
 import {getImage} from '@utils';
 
 type Props = {
@@ -14,9 +13,13 @@ type Props = {
 
 export const HorizontalCatalogItem: React.FC<Props> = ({item}) => {
   const navigation = useNavigation();
+
   const pressHandler = useCallback(() => {
     return null;
   }, [item, navigation]);
+
+  const countNew =
+    item.CNT_NEW && parseInt(item.CNT_NEW, 10) ? parseInt(item.CNT_NEW, 10) : 0;
 
   return (
     <Block
@@ -25,6 +28,15 @@ export const HorizontalCatalogItem: React.FC<Props> = ({item}) => {
       elevation={4}
       borderRadius={10}
       overflow={true}>
+      {countNew > 0 && (
+        <CntNew
+          justifyContent="center"
+          alignItems="center"
+          borderRadius={18}
+          backgroundColor={Colors.mainPrimary}>
+          <Typography.B16 color={Colors.white}>{item.CNT_NEW}</Typography.B16>
+        </CntNew>
+      )}
       <StyledPressable onPress={pressHandler}>
         <StyledImage resizeMode="contain" source={getImage(item.PICTURE)} />
         <Title color={Colors.black}>{item.NAME}</Title>
@@ -55,4 +67,12 @@ const Title = styled(Typography.B14)`
   bottom: 12px;
   background-color: rgba(255, 255, 255, 0.5);
   padding: 3px 10px;
+`;
+
+const CntNew = styled(Block)`
+  width: 36px;
+  height: 36px;
+  position: absolute;
+  top: 5px;
+  right: 5px;
 `;
--- src/screens/CatalogItemScreen/SmallCatalogItem.tsx
+++ src/screens/CatalogItemScreen/SmallCatalogItem.tsx
@@ -16,24 +16,42 @@ export const SmallCatalogItem: React.FC<Props> = ({item}) => {
   const pressHandler = useCallback(() => {
     return null;
   }, [item, navigation]);
-  const renderItem = useCallback((card: ICatalogItem) => {
-    // @ts-ignore
-    return (
-      <Block
-        width={'48.6%'}
-        backgroundColor={Colors.white}
-        elevation={4}
-        borderRadius={10}
-        overflow={true}>
-        <StyledPressable onPress={pressHandler}>
-          <StyledImage resizeMode="contain" source={getImage(card.PICTURE)} />
-          <Typography.B14 textAlign="center" color={Colors.black}>
-            {card.NAME}
-          </Typography.B14>
-        </StyledPressable>
-      </Block>
-    );
-  }, []);
+  const renderItem = useCallback(
+    (card: ICatalogItem) => {
+      const countNew =
+        card.CNT_NEW && parseInt(card.CNT_NEW, 10)
+          ? parseInt(card.CNT_NEW, 10)
+          : 0;
+
+      return (
+        <Wrapper
+          backgroundColor={Colors.white}
+          elevation={4}
+          borderRadius={10}
+          overflow={true}>
+          {countNew > 0 && (
+            <CntNew
+              justifyContent="center"
+              alignItems="center"
+              borderRadius={18}
+              backgroundColor={Colors.mainPrimary}>
+              <Typography.B16 color={Colors.white}>
+                {card.CNT_NEW}
+              </Typography.B16>
+            </CntNew>
+          )}
+          <StyledPressable onPress={pressHandler}>
+            <StyledImage resizeMode="contain" source={getImage(card.PICTURE)} />
+            <Typography.B14 textAlign="center" color={Colors.black}>
+              {card.NAME}
+            </Typography.B14>
+          </StyledPressable>
+        </Wrapper>
+      );
+    },
+    [pressHandler],
+  );
+
   return (
     <Row margin={5} justifyContent="space-between">
       {item.map(renderItem)}
@@ -57,4 +75,16 @@ const StyledImage = styled(Image)`
   width: 100px;
   height: 100px;
   margin-bottom: 10px;
+`;
+
+const CntNew = styled(Block)`
+  width: 36px;
+  height: 36px;
+  position: absolute;
+  top: 5px;
+  right: 5px;
+`;
+
+const Wrapper = styled(Block)`
+  width: 48.6%;
 `;
