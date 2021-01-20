c232d7b3074297783b371db63aefa27df33a33b2
Alexey <alex303606@gmail.com>
2021-01-20 23:24:12 +0200

    added splash and icon

---
+++ android/app/src/main/java/com/deliveryapp/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.deliveryapp; // Change this to your package name.
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        int SPLASH_DISPLAY_LENGTH = 0;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the MainActivity. */
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
